37266563b6e5aa8acda7680989f66918f5536cb8
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-07-04 22:27:58 +0100

    make pip-installable

---
+++ ahw/__init__.py
@@ -0,0 +1,11 @@
+from .native import greeting
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+
+    def build(self):
+        return Label(text = greeting())
+
+def main():
+    MyApp().run()
--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
-from setuptools import setup
+from Cython.Build import cythonize
+from pathlib import Path
+from setuptools import find_packages, setup
 
 setup(
-    install_requires = ['Kivy'],
+    entry_points = {'console_scripts': ['android-hello=ahw:main']},
+    ext_modules = cythonize(str(Path('ahw', 'native.pyx'))),
+    install_requires = ['Kivy'], # TODO: Use specific version.
+    name = 'android-hello-world',
+    packages = find_packages(),
 )
