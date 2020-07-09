9df1c1f886d38918fd984b6d094269d03d2fe882
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-07-10 00:23:09 +0100

    specific to apk

--- setup.py
+++ setup.py
@@ -3,10 +3,6 @@ from setuptools import Command, find_packages, setup
 from warnings import warn
 import os, subprocess
 
-mirror_volume = 'mirror'
-mirror_path = Path('build', 'Cowpox-mirror') # TODO: Do not guess where Cowpox expects it to be.
-src_path = '/src'
-
 class APK(Command):
 
     user_options = []
@@ -17,13 +13,17 @@ class APK(Command):
     def finalize_options(self):
         pass
 
+    mirror_volume = 'mirror'
+    mirror_path = Path('build', 'Cowpox-mirror') # TODO: Do not guess where Cowpox expects it to be.
+    src_path = '/src'
+
     def run(self):
-        mirror_path.mkdir(parents = True, exist_ok = True)
+        self.mirror_path.mkdir(parents = True, exist_ok = True)
         subprocess.check_call([
             'docker', 'run', '--rm', '-it',
-            '-v', f"{Path.cwd()}:{src_path}",
-            '-v', f"{mirror_volume}:{src_path}/{str(mirror_path).replace(os.sep, '/')}",
-            'combatopera/cowpox', src_path,
+            '-v', f"{Path.cwd()}:{self.src_path}",
+            '-v', f"{self.mirror_volume}:{self.src_path}/{str(self.mirror_path).replace(os.sep, '/')}",
+            'combatopera/cowpox', self.src_path,
         ])
 
 def cythonize(*args, **kwargs):
