2cf64e84fd5768940e954949a83d233f30a9540f
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-07-11 17:35:26 +0100

    abandon apk command, too much effort to make it work with plain old python and have a typical setup.py

---
+++ make.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import os
+
+mirror_volume = Path.home() / '.Cowpox' / 'mirror'
+mirror_relpath = Path('build', 'Cowpox-mirror') # TODO: Do not guess where Cowpox expects it to be.
+src_path = '/src'
+
+def main():
+    for d in mirror_volume, mirror_relpath:
+        d.mkdir(parents = True, exist_ok = True)
+    command = [
+        'docker', 'run', '--rm', '-it',
+        '-v', f"{Path.cwd()}:{src_path}",
+        '-v', f"{mirror_volume}:{src_path}/{str(mirror_relpath).replace(os.sep, '/')}",
+        'combatopera/cowpox', src_path,
+    ]
+    os.execvp(command[0], command)
+
+if '__main__' == __name__:
+    main()
--- setup.py
+++ setup.py
@@ -1,50 +1,9 @@
-from pathlib import Path
-from setuptools import Command, find_packages, setup
-from tempfile import NamedTemporaryFile
-from urllib.request import urlopen
-from zipfile import ZipFile
-import os, shutil, subprocess
+from setuptools import find_packages, setup
+import Cython.Build
 
-def import_cythonize():
-    try:
-        global cythonize
-        from Cython.Build import cythonize
-    except ModuleNotFoundError:
-        with NamedTemporaryFile() as g:
-            with urlopen('https://files.pythonhosted.org/packages/dd/22/f5573c59f64f935c7153b58f1ceeaa1938cf8d22f71950d64fc56e148db7/pyven-44-py2.py3-none-any.whl') as f:
-                shutil.copyfileobj(f, g)
-            with ZipFile(g.name) as zf, zf.open('pyven/cythonize.py') as f:
-                exec(f.read(), globals()) # Lazy cythonize so plain old Python can run apk command.
-
-class APK(Command):
-
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    mirror_volume = Path.home() / '.Cowpox' / 'mirror'
-    mirror_relpath = Path('build', 'Cowpox-mirror') # TODO: Do not guess where Cowpox expects it to be.
-    src_path = '/src'
-
-    def run(self):
-        for d in self.mirror_volume, self.mirror_relpath:
-            d.mkdir(parents = True, exist_ok = True)
-        subprocess.check_call([
-            'docker', 'run', '--rm', '-it',
-            '-v', f"{Path.cwd()}:{self.src_path}",
-            '-v', f"{self.mirror_volume}:{self.src_path}/{str(self.mirror_relpath).replace(os.sep, '/')}",
-            'combatopera/cowpox', self.src_path,
-        ])
-
-import_cythonize()
 setup(
-    cmdclass = {'apk': APK},
     entry_points = {'console_scripts': ['android-hello=ahw:main']},
-    ext_modules = cythonize('ahw/**/*.pyx'),
+    ext_modules = Cython.Build.cythonize('ahw/**/*.pyx'),
     install_requires = ['Kivy'], # TODO: Use specific version.
     name = 'android-hello-world',
     packages = find_packages(),
