4d2af49fa3b4e9ef30a1e947078e25de31ba276b
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-07-11 13:39:21 +0100

    basic seq protocol

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ def cythonize(*args, **kwargs):
     def init(ext_modules):
         from Cython.Build import cythonize
         ext_modules[:] = cythonize(*args, **kwargs)
-    return lazy(list, init, '__iter__', '__len__')
+    return lazy(list, init, '__getitem__', '__iter__', '__len__')
 
 setup(
     cmdclass = {'apk': APK},
