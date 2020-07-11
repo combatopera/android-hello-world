bc18c32d683b12cc1c06ff94e8f23272426bacec
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-07-11 17:51:43 +0100

    refuse to modify a partial venv

--- launch.py
+++ launch.py
@@ -10,6 +10,7 @@ def main():
     bin_dir = env_dir / 'bin'
     command = [bin_dir / 'android-hello']
     if not command[0].exists():
+        assert not env_dir.exists()
         venv.create(env_dir, with_pip = True)
         install = lambda *args: subprocess.check_call([bin_dir / 'pip', 'install', *args])
         install('--upgrade', 'pip')
