dee21f403c7387b872abbaade8c77f7ac4109c2a
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-07-11 06:50:25 +0100

    do not use named volume, too difficult to set correct uid/gid on its root dir

--- setup.py
+++ setup.py
@@ -13,16 +13,17 @@ class APK(Command):
     def finalize_options(self):
         pass
 
-    mirror_volume = 'mirror'
-    mirror_path = Path('build', 'Cowpox-mirror') # TODO: Do not guess where Cowpox expects it to be.
+    mirror_volume = Path.home() / '.Cowpox' / 'mirror'
+    mirror_relpath = Path('build', 'Cowpox-mirror') # TODO: Do not guess where Cowpox expects it to be.
     src_path = '/src'
 
     def run(self):
-        self.mirror_path.mkdir(parents = True, exist_ok = True)
+        for d in self.mirror_volume, self.mirror_relpath:
+            d.mkdir(parents = True, exist_ok = True)
         subprocess.check_call([
             'docker', 'run', '--rm', '-it',
             '-v', f"{Path.cwd()}:{self.src_path}",
-            '-v', f"{self.mirror_volume}:{self.src_path}/{str(self.mirror_path).replace(os.sep, '/')}",
+            '-v', f"{self.mirror_volume}:{self.src_path}/{str(self.mirror_relpath).replace(os.sep, '/')}",
             'combatopera/cowpox', self.src_path,
         ])
 
