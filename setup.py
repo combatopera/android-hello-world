82621133897230039104a9ebbeacef946634e992
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-07-12 16:15:12 +0100

    make warning go away

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import Cython.Build
 
 setup(
     entry_points = {'console_scripts': ['android-hello=ahw:main']},
-    ext_modules = Cython.Build.cythonize('ahw/**/*.pyx'),
+    ext_modules = Cython.Build.cythonize('ahw/**/*.pyx', compiler_directives = {'language_level': 3}),
     install_requires = ['Kivy'], # TODO: Use specific version.
     name = 'android-hello-world',
     packages = find_packages(),
