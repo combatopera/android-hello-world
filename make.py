1d423c8eafad503631ff3097274dbd4638248e28
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-07-17 21:41:31 +0100

    we can mount the mirror anywhere we like

--- make.py
+++ make.py
@@ -4,20 +4,19 @@ from pathlib import Path
 import os
 
 host_mirror = Path.home() / '.Cowpox' / 'mirror'
-mirror_relpath = Path('build', 'Cowpox-mirror')
+container_mirror = '/mirror'
 container_src = '/src'
 
 def main():
     'Example for building the APK.'
-    for d in host_mirror, mirror_relpath:
-        d.mkdir(parents = True, exist_ok = True)
+    host_mirror.mkdir(parents = True, exist_ok = True)
     command = [
         'docker', 'run', '--rm', '-it',
         '-v', f"{Path.cwd()}:{container_src}",
-        '-v', f"{host_mirror}:{container_src}/{str(mirror_relpath).replace(os.sep, '/')}",
+        '-v', f"{host_mirror}:{container_mirror}",
         'combatopera/cowpox',
         f"container src = {container_src}",
-        f"mirror path = {container_src}/{str(mirror_relpath).replace(os.sep, '/')}",
+        f"mirror path = {container_mirror}",
     ]
     os.execvp(command[0], command)
 
