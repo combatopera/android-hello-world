cb817b69abe27823a8213f90b1adebf37b5b5c23
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-11-26 23:56:07 +0000

    lower bound

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import Cython.Build
 setup(
     entry_points = {'console_scripts': ['android-hello=ahw:main']},
     ext_modules = Cython.Build.cythonize('ahw/**/*.pyx', compiler_directives = {'language_level': 3}),
-    install_requires = ['Kivy'], # TODO: Use specific version.
+    install_requires = ['Kivy>=1.11.1'],
     name = 'android-hello-world',
     packages = find_packages(),
 )
