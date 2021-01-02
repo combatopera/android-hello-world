5a65c66dbb03f01e6ef0b25dac120dcecb6fac4d
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-01-02 19:22:06 +0000

    bit conciser

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import find_packages, setup
-import Cython.Build
+from Cython.Build import cythonize
 
 setup(
     entry_points = {'console_scripts': ['android-hello=ahw:main']},
-    ext_modules = Cython.Build.cythonize('ahw/**/*.pyx', compiler_directives = {'language_level': 3}),
+    ext_modules = cythonize('ahw/**/*.pyx', compiler_directives = {'language_level': 3}),
     install_requires = ['Kivy>=2'],
     name = 'android-hello-world',
     packages = find_packages(),
